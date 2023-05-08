e3d447c2becb92ad5ed74909e32ffb8ea662fff1
arnstgutan <arnstsasha92@gmail.com>
2023-05-09 02:01:24 +0300

    init: start node tasks and done 4 of 6

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require("path");
+const { readdir, copyFile, mkdir, rm } = require("fs/promises");
+const { stdout } = require("process");
+const copyForm = path.join(__dirname, "files");
+const copyTo = path.join(__dirname, "files-copy");
 
+async function copy(copyForm, copyTo) {
+  try {
+    const dirFiles = await readdir(copyForm, { withFileTypes: true });
+    for (const el of dirFiles) {
+      if (el.isFile()) {
+        await copyFile(
+          path.join(copyForm, el.name),
+          path.join(copyTo, el.name)
+        );
+      } else if (el.isDirectory()) {
+        await mkdir(path.join(copyForm), el.name);
+        await copy(path.join(copyForm, el.name), path.join(copyTo, el.name));
+      }
+    }
+  } catch (error) {
+    stdout.write(`Error: ${error.massage}\n`);
+  }
+}
+
+(async function () {
+  await rm(copyTo, { recursive: true, force: true });
+  await mkdir(copyTo, { recursive: true });
+  await copy(copyForm, copyTo);
+})();
